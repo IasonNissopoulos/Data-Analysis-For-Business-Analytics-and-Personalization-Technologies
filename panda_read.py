a491729782fb24a7373993d9d2ff79d1cf1cc9c5
iliasTsiortas <iliasmorton96@gmail.com>
2019-01-04 15:50:24 +0200

    Cleaned negative numbers on Sum_Units and Sum_value

--- panda_read.py
+++ panda_read.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import xlsxwriter
 
 xls = pd.ExcelFile('POS DATA_BAPT.xlsx')
 df1 = pd.read_excel(xls, 'POS DATA')
@@ -7,4 +8,13 @@ df2 = pd.read_excel(xls, 'LOYALTY')
 df3 = pd.read_excel(xls, 'BARCODES')
 df4 = pd.read_excel(xls, 'Categories Hierarchy')
 
-print(df1.loc[: , "Date"])
+df1 = df1[df1["Sum_Units"] >= 0]
+
+writer = pd.ExcelWriter('POS DATA_BAPT.xlsx', engine='xlsxwriter')
+
+df1.to_excel(writer, sheet_name='POS DATA')
+df2.to_excel(writer, sheet_name='LOYALTY')
+df3.to_excel(writer, sheet_name='BARCODES')
+df4.to_excel(writer, sheet_name='Categories Hierarchy')
+
+writer.save()
